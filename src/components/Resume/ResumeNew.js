d92cc10b58137ca82eb82a160d5fd285bbb1eae5
Shivansh <sgshivansh22@gmail.com>
2024-05-23 15:22:50 +0530

    My resume uploaded

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -4,6 +4,7 @@ import Button from "react-bootstrap/Button";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+import pdf from "../../Assets/Shivansh_Resume.pdf";
 import Particle from "../Particle";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
